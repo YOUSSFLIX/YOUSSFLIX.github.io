d203e77679fa2b0f47861b02210cf143df1a445b
YOUSSFLIX <147429671+nokia590@users.noreply.github.com>
2024-06-20 09:11:21 -0700

    Update facebook.js

--- assets/js/facebook.js
+++ assets/js/facebook.js
@@ -1,47 +1,35 @@
-window.fbAsyncInit = function () {
-    // Initialize the SDK with your app and the Graph API version for your app 
+
+<script>
+  window.fbAsyncInit = function() {
     FB.init({
-        appId: '357140426152444',
-        xfbml: true,
-        version: 'v19.0',
-        status: true
+      appId      : '772407911757611',
+      cookie     : true,
+      xfbml      : true,
+      version    : 'v20.0'
     });
-};
+      
+    FB.AppEvents.logPageView();   
+      
+  };
 
-// Add the Facebook SDK for Javascript
-(function (d, s, id) {
-    var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) { return; }
-    js = d.createElement(s); js.id = id;
-    js.src = "https://connect.facebook.net/fr_FR/sdk.js";
-    js.onerror = function () {
-        showError("Veuillez désactiver votre bloqueur de pubs pour permettre l'accès à Facebook.")
-    };
-    fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "https://connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+</script>
 
-function isFBConnected() {
-    return new Promise((resolve, reject) => {
-        FB.getLoginStatus(function (response) {
-            if (response.status === 'connected') {
-                resolve(response.authResponse.accessToken)
-            } else {
-                resolve(false)
-            }
-        });
-    })
-}
-
-const NETFLIXY_PAGE_ID = '275781865624176'
-
-function isPageLiked() {
-    return new Promise((resolve, _reject) => {
-        FB.api('/me/likes/' + NETFLIXY_PAGE_ID, function (response) {
-            if (response.data.length > 0) {
-                resolve(true)
-            } else {
-                resolve(false)
-            }
-        });
-    })
+FB.getLoginStatus(function(response) {
+    statusChangeCallback(response);
+});
+{
+    status: 'connected',
+    authResponse: {
+        accessToken: '...',
+        expiresIn:'...',
+        signedRequest:'...',
+        userID:'...'
+    }
 }
