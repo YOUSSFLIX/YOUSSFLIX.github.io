1410442805cdc82bc5ed1ff32bb5d877de4dc3ae
Ysfnokia <147429671+YOUSSFLIX@users.noreply.github.com>
2024-07-04 11:36:19 -0700

    Update facebook.js

--- assets/js/facebook.js
+++ assets/js/facebook.js
@@ -1,24 +1,25 @@
-window.fbAsyncInit = function () {
-    // Initialize the SDK with your app and the Graph API version for your app 
-    FB.init({
-        appId: '929867048907751',
-        xfbml: true,
-        version: 'v20.0',
-        status: true
-    });
-};
 
-// Add the Facebook SDK for Javascript
-(function (d, s, id) {
-    var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) { return; }
-    js = d.createElement(s); js.id = id;
-    js.src = "https://connect.facebook.net/fr_FR/sdk.js";
-    js.onerror = function () {
-        showError("Veuillez désactiver votre bloqueur de pubs pour permettre l'accès à Facebook.")
-    };
-    fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
+<script>
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '929867048907751',
+      cookie     : true,
+      xfbml      : true,
+      version    : '{api-version}'
+    });
+      
+    FB.AppEvents.logPageView();   
+      
+  };
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "https://connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+</script>
 
 function isFBConnected() {
     return new Promise((resolve, reject) => {
