e934f61e5788d73bd1e1dbfad31e6362dc13977f
Ysfnokia <147429671+YOUSSFLIX@users.noreply.github.com>
2024-07-04 11:45:48 -0700

    Update facebook.js

--- assets/js/facebook.js
+++ assets/js/facebook.js
@@ -1,6 +1,7 @@
 
 <script>
   window.fbAsyncInit = function() {
+    // Initialize the SDK with your app and the Graph API version for your app 
     FB.init({
       appId      : '929867048907751',
       cookie     : true,
@@ -12,14 +13,17 @@
       
   };
 
-  (function(d, s, id){
-     var js, fjs = d.getElementsByTagName(s)[0];
-     if (d.getElementById(id)) {return;}
-     js = d.createElement(s); js.id = id;
-     js.src = "https://connect.facebook.net/en_US/sdk.js";
-     fjs.parentNode.insertBefore(js, fjs);
-   }(document, 'script', 'facebook-jssdk'));
-</script>
+// Add the Facebook SDK for Javascript
+(function (d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) { return; }
+    js = d.createElement(s); js.id = id;
+    js.src = "https://connect.facebook.net/fr_FR/sdk.js";
+    js.onerror = function () {
+        showError("Veuillez désactiver votre bloqueur de pubs pour permettre l'accès à Facebook.")
+    };
+    fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
 
 function isFBConnected() {
     return new Promise((resolve, reject) => {
@@ -46,3 +50,4 @@ function isPageLiked() {
         });
     })
 }
+
